480a59b310b7121639adcbf9b732025da7b7ea63
ale_pashkin <alepas1307@gmail.com>
2022-09-16 19:32:41 +0300

    5_62

---
+++ 5_62/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая заполнит спирально массив 4 на 4.
+
+int[,] array = new int[4, 4];
+
+int num = 1;
+int i = 0;
+int j = 0;
+
+while (num <= 4 * 4)
+{
+    array[i, j] = num;
+    if (i <= j + 1 && i + j < 4 - 1) ++j;
+    else if (i < j && i + j >= 4 - 1) ++i;
+    else if (i >= j && i + j > 4 - 1) --j;
+    else --i;
+    ++num;
+}
+
+PrintArray(array);
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
